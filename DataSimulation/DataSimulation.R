9b0e0c30d9fbd7ee0d45881df03c99ee30c3aa65
Tim Lindner <tim.lindner.1994@web.de>
2024-03-25 21:04:05 +0100

    Create DataSimulation

---
+++ DataSimulation/DataSimulation.R
@@ -0,0 +1,70 @@
+# closing the sections provides an overview of the script
+
+
+# preparation ####
+# set working directory
+setwd("C:/Users/Diiim/Documents/ResearchAssistance/DataSimulation")
+
+# clean workspace
+rm(list = ls())
+
+# clean garbage
+gc()
+
+# set decimals to digits instead of scientific
+options(scipen = 999)
+
+# load packages
+library(openxlsx)
+library(readxl)
+
+
+# model 1 two classes ####
+# number of individuals
+N <- 30
+
+# number of periods
+p <- 0:9
+number_of_periods <- length(p)
+
+# number of latent classes
+C <- 2
+
+# constants
+beta_0 <- c(-2,2)
+
+# linear trends
+beta_1 <- c(-0.5,0.5)
+
+# standard deviations for Normal distributions
+sigma <- c(1,1)
+
+# mixture proportions
+Pi <- matrix(data = NA, nrow = N, ncol = C)
+for (n in 1:round(N/2)) {
+  Pi[n,] <- c(0.9,0.1)
+}
+for (n in (round(N/2)+1):N) {
+  Pi[n,] <- c(0.1,0.9)
+}
+
+# simulated dependent variable
+Y_sim <- matrix(data = 0, nrow = N, ncol = number_of_periods)
+for (n in 1:N) {
+  for (t in 1:number_of_periods) {
+    for (c in 1:C) {
+      mu <- beta_0[c] + beta_1[c] * p[t]  # means for Normal distributions
+      Y_sim[n,t] <-
+        Y_sim[n,t] + Pi[n,c] * rnorm(n = 1, mean = mu, sd = sigma[c])
+    }
+  }
+}
+
+# save simulated dependent variable
+write.xlsx(R_act, "Model1_TwoClasses_Y.xlsx")
+
+# load simulated dependent variable
+Y_sim <- read_excel("DataSimulation/Model1_TwoClasses_Y.xlsx",
+                    sheet = "Sheet 1")
+
+
