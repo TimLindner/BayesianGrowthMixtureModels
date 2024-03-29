44ede65fe6aae770bb4d8260658ec714878ae4de
Tim Lindner <tim.lindner.1994@web.de>
2024-03-29 21:47:17 +0100

    Create DataSimulation.R

---
+++ SimulationStudyData/DataSimulation.R
@@ -0,0 +1,118 @@
+# closing the sections provides an overview of the script
+
+
+# preparation ####
+# set working directory
+setwd("C:/Users/Diiim/Documents/ResearchAssistance/DataSimulation")
+
+# clean workspace
+rm(list = ls())
+
+# clean garbage
+gc()
+
+# set decimals to digits instead of scientific
+options(scipen = 999)
+
+# load packages
+library(openxlsx)
+library(readxl)
+
+
+# model 1 general ####
+# number of individuals
+N <- 30
+
+# number of time periods
+no_periods <- 10
+
+# time periods
+time_periods <- 0:(no_periods - 1)
+X <- matrix(time_periods, nrow = N, ncol = no_periods, byrow = TRUE)
+
+
+# model 1 baseline ####
+# constant
+beta_0_sim <- 5
+
+# linear trend
+beta_1_sim <- 0.5
+
+# means for Normal distributions
+M_sim <- matrix(data = 0, nrow = N, ncol = no_periods) 
+for (t in 1:no_periods) {
+  M_sim[,t] <- beta_0_sim + beta_1_sim * X[,t]
+}
+
+# standard deviation for Normal distributions
+sigma_sim <- 0.75
+
+# simulated dependent variable
+Y_sim <- matrix(data = 0, nrow = N, ncol = no_perios)
+for (t in 1:no_periods) {
+  Y_sim[,t] <- rnorm(n = N, mean = M_sim[,t], sd = sigma_sim)
+}
+
+# save Y_sim ( transformed to data frame beforehand )
+write.xlsx(data.frame(Y_sim), "Model1Baseline_Ysim.xlsx")
+
+# load Y_sim
+Y_sim <- read_excel("Model1Baseline_Ysim.xlsx",
+                    sheet = "Sheet 1")
+
+
+# model 1 two classes ####
+# number of latent classes
+C <- 2
+
+# constants
+beta_0 <- c(-5,5)
+
+# linear trends
+beta_1 <- c(-0.5,0.5)
+
+# means for Normal distributions step 1
+M_sim_mtx <- matrix(data = 0, nrow = N, ncol = C)
+M_sim <- list()
+for (t in 1:T) {
+  M_sim[[t]] <- M_sim_mtx
+}
+
+# means for Normal distributions step 1
+for (t in 1:no_periods) {
+  for (c in 1:C) {
+    M_sim[[t]][,c] <- beta_0_sim[c] + beta_1_sim[c] * X[,t]
+  }
+}
+
+# standard deviations for Normal distributions
+sigma <- c(0.25,0.75)
+
+# mixture proportions step 1
+Pi <- matrix(data = 0, nrow = N, ncol = C)
+for (n in 1:round(N/2)) {
+  Pi[n,] <- c(0.9,0.1)
+}
+
+# mixture proportions step 2
+for (n in (round(N/2)+1):N) {
+  Pi[n,] <- c(0.1,0.9)
+}
+
+# simulated dependent variable
+Y_sim <- matrix(data = 0, nrow = N, ncol = no_periods)
+for (t in 1:no_periods) {
+  for (c in 1:C) {
+    Y_sim[,t] <-
+      Y_sim[,t] + Pi[,c] * rnorm(n = N, mean = M_sim[[t]][,c], sd = sigma[c])
+  }
+}
+
+# save Y_sim ( transformed to data frame beforehand )
+write.xlsx(data.frame(Y_sim), "Model1TwoClasses_Ysim.xlsx")
+
+# load Y_sim
+Y_sim <- read_excel("Model1TwoClasses_Ysim.xlsx",
+                    sheet = "Sheet 1")
+
+
