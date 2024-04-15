673700431418d9af5b377b751f0c8dfac1c93b54
Tim Lindner <tim.lindner.1994@web.de>
2024-04-15 16:26:21 +0200

    Update ModelEstimations.R

--- SimulationStudyResults/ModelEstimations.R
+++ SimulationStudyResults/ModelEstimations.R
@@ -22,6 +22,10 @@
 # Dataset4_Yobs.xlsx
 # Model1MultipleClasses.stan
 
+# required files for model 1 three classes - dataset 5 section:
+# Dataset5_Yobs.xlsx
+# Model1MultipleClasses.stan
+
 
 # preparation ####
 # set working directory
@@ -76,9 +80,6 @@ sigma_prior_sigma <- 1
 # load model
 m <- stan_model("ModelImplementations/Model1Baseline.stan")
 
-# sampler: algorithm
-algorithm <- "NUTS"
-
 # sampler: number of chains
 chains <- 4
 
@@ -88,8 +89,11 @@ iter <- 2000
 # sampler: number of warmup iterations per chain
 warmup <- floor(iter/2)
 
-# sampler: parameter initialization
+# sampler: initial values for parameters
 init <- "random"
+
+# sampler: algorithm
+algorithm <- "NUTS"
 
 job::job({
   
@@ -139,7 +143,7 @@ X <- matrix(data = time_periods, nrow = N, ncol = no_periods, byrow = TRUE)
 k_means <- kmeans(Y_obs[,1],  # K-means clustering
                   centers = C,
                   iter.max = 10,
-                  nstart = 1,
+                  nstart = 2,
                   algorithm = "Hartigan-Wong")
 beta_0_prior_mu <- sort(k_means$centers)
 
@@ -158,9 +162,6 @@ sigma_prior_sigma <- c(0.5,0.5)
 # load model
 m <- stan_model("ModelImplementations/Model1MultipleClasses.stan")
 
-# sampler: algorithm
-algorithm <- "NUTS"
-
 # sampler: number of chains
 chains <- 4
 
@@ -170,8 +171,11 @@ iter <- 2000
 # sampler: number of warmup iterations per chain
 warmup <- floor(iter/2)
 
-# sampler: parameter initialization
+# sampler: initial values for parameters
 init <- "random"
+
+# sampler: algorithm
+algorithm <- "NUTS"
 
 job::job({
   
@@ -218,7 +222,12 @@ time_periods <- 0:(no_periods-1)
 X <- matrix(data = time_periods, nrow = N, ncol = no_periods, byrow = TRUE)
 
 # mean hyperparameter for Normal prior of constant
-beta_0_prior_mu <- rep(mean(Y_obs[,1]), times = C)
+k_means <- kmeans(Y_obs[,1],  # K-means clustering
+                  centers = C,
+                  iter.max = 10,
+                  nstart = 2,
+                  algorithm = "Hartigan-Wong")
+beta_0_prior_mu <- sort(k_means$centers)
 
 # SD hyperparameters for Normal prior of constants
 beta_0_prior_sigma <- c(1,1)
@@ -235,9 +244,6 @@ sigma_prior_sigma <- c(0.5,0.5)
 # load model
 m <- stan_model("ModelImplementations/Model1MultipleClasses.stan")
 
-# sampler: algorithm
-algorithm <- "NUTS"
-
 # sampler: number of chains
 chains <- 4
 
@@ -247,8 +253,11 @@ iter <- 2000
 # sampler: number of warmup iterations per chain
 warmup <- floor(iter/2)
 
-# sampler: parameter initialization
+# sampler: initial values for parameters
 init <- "random"
+
+# sampler: algorithm
+algorithm <- "NUTS"
 
 job::job({
   
@@ -298,7 +307,7 @@ X <- matrix(data = time_periods, nrow = N, ncol = no_periods, byrow = TRUE)
 k_means <- kmeans(Y_obs[,1],  # K-means clustering
                   centers = C,
                   iter.max = 10,
-                  nstart = 1,
+                  nstart = 2,
                   algorithm = "Hartigan-Wong")
 beta_0_prior_mu <- sort(k_means$centers)
 
@@ -317,9 +326,6 @@ sigma_prior_sigma <- c(0.5,0.5)
 # load model
 m <- stan_model("ModelImplementations/Model1MultipleClasses.stan")
 
-# sampler: algorithm
-algorithm <- "NUTS"
-
 # sampler: number of chains
 chains <- 4
 
@@ -329,8 +335,11 @@ iter <- 2000
 # sampler: number of warmup iterations per chain
 warmup <- floor(iter/2)
 
-# sampler: parameter initialization
+# sampler: initial values for parameters
 init <- "random"
+
+# sampler: algorithm
+algorithm <- "NUTS"
 
 job::job({
   
@@ -354,6 +363,91 @@ job::job({
   # save model fit
   saveRDS(m_fit,
           "SimulationStudyResults/Model1TwoClasses_Dataset4_Fit1.rds")
+  
+})
+
+
+# model 1 three classes - dataset 5 ####
+# number of latent classes
+C <- 3
+
+# load observed dependent variable
+Y_obs <- data.frame(read_excel("SimulationStudyData/Dataset5_Yobs.xlsx",
+                               sheet = "Sheet 1"))
+
+# number of individuals
+N <- dim(Y_obs)[1]
+
+# number of time periods
+no_periods <- dim(Y_obs)[2]
+
+# time periods
+time_periods <- 0:(no_periods-1)
+X <- matrix(data = time_periods, nrow = N, ncol = no_periods, byrow = TRUE)
+
+# mean hyperparameter for Normal prior of constant
+k_means <- kmeans(Y_obs[,1],  # K-means clustering
+                  centers = C,
+                  iter.max = 10,
+                  nstart = 2,
+                  algorithm = "Hartigan-Wong")
+beta_0_prior_mu <- sort(k_means$centers)
+
+# SD hyperparameters for Normal prior of constants
+beta_0_prior_sigma <- c(0.5,0.5,0.5)
+
+# mean hyperparameters for Normal prior of linear trend components
+beta_1_prior_mu <- c(0,0,0)
+
+# SD hyperparameters for Normal prior of linear trend components
+beta_1_prior_sigma <- c(1,1,1)
+
+# SD hyperparameters for Normal prior of SDs for Y Normal distributions
+sigma_prior_sigma <- c(0.5,0.5,0.5)
+
+# load model
+m <- stan_model("ModelImplementations/Model1MultipleClasses.stan")
+
+# sampler: number of chains
+chains <- 4
+
+# sampler: number of iterations per chain
+iter <- 2000
+
+# sampler: number of warmup iterations per chain
+warmup <- floor(iter/2)
+
+# sampler: initial values for parameters
+init <- list()
+for (ch in 1:chains) {
+  init[[ch]] <- list(beta_0 = beta_0_prior_mu)
+}
+
+# sampler: algorithm
+algorithm <- "NUTS"
+
+job::job({
+  
+  # estimate model
+  m_fit <- sampling(m,
+                    data = list(C = C,
+                                T = no_periods,
+                                Y_obs = Y_obs,
+                                X = X,
+                                beta_0_prior_mu = beta_0_prior_mu,
+                                beta_0_prior_sigma = beta_0_prior_sigma,
+                                beta_1_prior_mu = beta_1_prior_mu,
+                                beta_1_prior_sigma = beta_1_prior_sigma,
+                                sigma_prior_sigma = sigma_prior_sigma),
+                    chains = chains,
+                    iter = iter,
+                    warmup = warmup,
+                    init = init,
+                    algorithm = algorithm)
+  
+  # save model fit
+  saveRDS(m_fit,
+          "SimulationStudyResults/Model1ThreeClasses_Dataset5_Fit1.rds")
   
 })
 
