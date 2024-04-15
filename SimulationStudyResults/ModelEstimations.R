d3b6e3a17655ba97521b945eabb5c666cf08b78c
Tim Lindner <tim.lindner.1994@web.de>
2024-04-15 20:41:36 +0200

    Update ModelEstimations.R

--- SimulationStudyResults/ModelEstimations.R
+++ SimulationStudyResults/ModelEstimations.R
@@ -26,6 +26,10 @@
 # Dataset5_Yobs.xlsx
 # Model1MultipleClasses.stan
 
+# required files for model 1 three classes - dataset 6 section:
+# Dataset6_Yobs.xlsx
+# Model1MultipleClasses.stan
+
 
 # preparation ####
 # set working directory
@@ -448,6 +452,91 @@ job::job({
   # save model fit
   saveRDS(m_fit,
           "SimulationStudyResults/Model1ThreeClasses_Dataset5_Fit1.rds")
+  
+})
+
+
+# model 1 three classes - dataset 6 ####
+# number of latent classes
+C <- 3
+
+# load observed dependent variable
+Y_obs <- data.frame(read_excel("SimulationStudyData/Dataset6_Yobs.xlsx",
+                               sheet = "Sheet 1"))
+
+# number of individuals
+N <- dim(Y_obs)[1]
+
+# number of time periods
+no_periods <- dim(Y_obs)[2]
+
+# time periods
+time_periods <- 0:(no_periods-1)
+X <- matrix(data = time_periods, nrow = N, ncol = no_periods, byrow = TRUE)
+
+# mean hyperparameter for Normal prior of constant
+k_means <- kmeans(Y_obs[,1],  # K-means clustering
+                  centers = C,
+                  iter.max = 10,
+                  nstart = 2,
+                  algorithm = "Hartigan-Wong")
+beta_0_prior_mu <- sort(k_means$centers)
+
+# SD hyperparameters for Normal prior of constants
+beta_0_prior_sigma <- c(1,1,1)
+
+# mean hyperparameters for Normal prior of linear trend components
+beta_1_prior_mu <- c(0,0,0)
+
+# SD hyperparameters for Normal prior of linear trend components
+beta_1_prior_sigma <- c(1,1,1)
+
+# SD hyperparameters for Normal prior of SDs for Y Normal distributions
+sigma_prior_sigma <- c(0.5,0.5,0.5)
+
+# load model
+m <- stan_model("ModelImplementations/Model1MultipleClasses.stan")
+
+# sampler: number of chains
+chains <- 4
+
+# sampler: number of iterations per chain
+iter <- 2000
+
+# sampler: number of warmup iterations per chain
+warmup <- floor(iter/2)
+
+# sampler: initial values for parameters
+init <- list()
+for (ch in 1:chains) {
+  init[[ch]] <- list(beta_0 = beta_0_prior_mu)
+}
+
+# sampler: algorithm
+algorithm <- "NUTS"
+
+job::job({
+  
+  # estimate model
+  m_fit <- sampling(m,
+                    data = list(C = C,
+                                T = no_periods,
+                                Y_obs = Y_obs,
+                                X = X,
+                                beta_0_prior_mu = beta_0_prior_mu,
+                                beta_0_prior_sigma = beta_0_prior_sigma,
+                                beta_1_prior_mu = beta_1_prior_mu,
+                                beta_1_prior_sigma = beta_1_prior_sigma,
+                                sigma_prior_sigma = sigma_prior_sigma),
+                    chains = chains,
+                    iter = iter,
+                    warmup = warmup,
+                    init = init,
+                    algorithm = algorithm)
+  
+  # save model fit
+  saveRDS(m_fit,
+          "SimulationStudyResults/Model1ThreeClasses_Dataset6_Fit1.rds")
   
 })
 
