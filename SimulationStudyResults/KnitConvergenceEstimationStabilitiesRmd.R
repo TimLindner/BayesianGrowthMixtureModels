1ac019534893c5731237ed6313fedeebbef55c19
Tim Lindner <tim.lindner.1994@web.de>
2024-05-04 13:48:24 +0200

    Create KnitConvergenceEstimationStabilitiesRmd.R

---
+++ SimulationStudyResults/KnitConvergenceEstimationStabilitiesRmd.R
@@ -0,0 +1,25 @@
+# README
+# required file from SimulationStudyResults folder:
+# ConvergenceEstimationStabilities.Rmd
+
+# CTR + ALT + R runs the entire R script
+
+# set working directory
+setwd("C:/Users/Diiim/Documents/GitHub/BayesianGMMs/SimulationStudyResults")
+
+# clean workspace
+rm(list = ls())
+
+# clean garbage
+gc()
+
+# set decimals to digits instead of scientific
+options(scipen = 999)
+
+# load packages
+library(rmarkdown)
+
+# knit ConvergenceEstimationStabilities.Rmd
+rmarkdown::render("ConvergenceEstimationStabilities.Rmd")
+
+
